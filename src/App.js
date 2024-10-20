2219e50a677d703ef1cd3f22149ea4763dcab90a
Josephine Jacobs <133575092+JosephineJacobs@users.noreply.github.com>
2024-10-20 15:13:04 +0200

    basic structure set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return <div className='App'>Weather app</div>
-
-  
+export default function App() {
+  return (
+    <div className="App">
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/83802-josephine-lu-ching-jacobs"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Josephine Jacobs{" "}
+        </a>
+        and is open-sourced code on{" "}
+        <a
+          href="https://github.com/JosephineJacobs/weather-create-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+        , hosted by{" "}
+        <a
+          href="https://weatherappfinaljj.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify.
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
